bb7390b687be3c720e68689bcfb773bc3861b0c3
Balaji Shinnosuke <62749324+bluv-pr@users.noreply.github.com>
2021-04-27 09:40:41 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -50,7 +50,7 @@ class Progress:
             [
                 [
                     InlineKeyboardButton(
-                        "Cancel 🚫",
+                        "Cᴀɴᴄᴇʟ 😷",
                         callback_data=(
                             f"gUPcancel/{chat_id}/{mes_id}/{from_user}"
                         ).encode("UTF-8"),
